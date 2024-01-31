517e730011448af22e5a0a23e95d1d194c4e7202
Darren Jensen <darren.jensen@gmail.com>
2024-01-31 19:31:07 +0700

    completed timelock with tests

--- src/Timelock.sol
+++ src/Timelock.sol
@@ -1,22 +1,52 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.23;
 
-contract Timelock {
-    // overflow and underflow examples and preventions
-    // one can deposit ether into this contract but you must wait 1 week before you can withdraw your funds
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 
-    // calling SafeMath will add extra functions to the uint data type
+// one can deposit ether into this contract but you must wait 
+// 1 week before you can withdraw your funds
+
+contract Timelock {
+    // calling Math will add extra functions to the uint data type
+    using Math for uint; // you can make a call like myUint.tryAdd(123)
 
     // amount of ether you deposited is saved in balances
+    mapping(address => uint) public balances;
 
     // when you can withdraw is saved in lockTime
+    mapping(address => uint) public lockTime;
 
     // deposit() function
+    function deposit() external payable {
+        balances[msg.sender] += msg.value;
+
+        lockTime[msg.sender] = block.timestamp + 1 weeks;
+    }
 
     // increaseLockTime(_secondsToIncrease) function
+    function increaseLockTime(uint _secondsToIncrease) external {
+        (bool success, uint result) = lockTime[msg.sender].tryAdd(_secondsToIncrease);
+        if (success) {
+            // update the callers lockTime as requested!
+            lockTime[msg.sender] = result;
+        }
+    }
 
     // withdraw() function
-
+    // follow checks-effects-interactions! :) 
+    function withdraw() external {
         // check that the sender has ether deposited in this contract in the mapping and the balance is >0
+        require(balances[msg.sender] > 0, "Zero balance");
         // check that the now time is > the time saved in the lock time mapping
+        require(block.timestamp >= lockTime[msg.sender], "Locked balance");
+
+        // effects - update the balances
+        uint amount = balances[msg.sender];
+        balances[msg.sender] = 0;
+
+        // interactions - send funds to caller
+        (bool success, ) = msg.sender.call{value: amount}("");
+        require(success, "transfer failed");
+    }
+
 }
--- test/Timelock.t.sol
+++ test/Timelock.t.sol
@@ -5,9 +5,62 @@ import {Test} from "forge-std/Test.sol";
 import {console} from "forge-std/Console.sol";
 import {Timelock} from "../src/Timelock.sol";
 
-contract HotelRoomTest is Test {
+abstract contract TimelockTestBase is Test {
     Timelock tl;
-    function setUp() public {
+    address user1 = makeAddr("user1");
+    function setUp() public virtual {
         tl = new Timelock();
+    }
+}
+
+contract TimeLockTest is TimelockTestBase {
+    function test_Deposit() public {
+        hoax(user1, 10 ether);
+        tl.deposit{value: 3 ether}();
+        assertEq(tl.balances(user1), 3 ether);
+        assertEq(tl.lockTime(user1), block.timestamp + 1 weeks);
+    }   
+
+    function test_WithdrawZeroBalance() public {
+        vm.expectRevert("Zero balance");   
+        vm.prank(user1);
+        tl.withdraw();
+    }
+}
+
+contract TimeLockDepositedTest is TimelockTestBase {
+    function setUp() public override {
+        super.setUp();
+        hoax(user1, 10 ether);
+        tl.deposit{value: 3 ether}();
+    }
+
+    function test_Deposit() public {
+        vm.prank(user1);
+        tl.deposit{value: 1 ether}();
+        assertEq(tl.balances(user1), 4 ether);
+        assertEq(tl.lockTime(user1), block.timestamp + 1 weeks);
+    }   
+
+     function test_increaseLockTime() public {
+        uint preLockTime = tl.lockTime(user1);
+        vm.prank(user1);
+        tl.increaseLockTime(1 hours);
+        assertEq(tl.lockTime(user1), preLockTime + 1 hours);
+    }
+
+    function test_WithdrawEarly() public {
+        vm.expectRevert("Locked balance");
+        vm.prank(user1);
+        tl.withdraw();
+    }
+
+    function test_WithdrawSuccess() public {
+        assertEq(address(user1).balance, 7 ether);
+        uint unlockTime = tl.lockTime(user1);
+        vm.warp(unlockTime);
+        vm.prank(user1);
+        tl.withdraw();
+        assertEq(address(user1).balance, 10 ether);
     }
 }
